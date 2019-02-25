5beec458ac61e401d9d5c4a19b5459514a780be0
Briank428 <44040150+Briank428@users.noreply.github.com>
2019-02-25 11:08:20 -0500

    Worked on gamemanager

--- Hats/Assets/Scripts/GameManager.cs
+++ Hats/Assets/Scripts/GameManager.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public struct Hats
+{
+    float probability;
+    string type;
+    Sprite image;
+}
 public class GameManager : MonoBehaviour
 {
+    public List<Hats> hatTypes;
+    public GameObject anvilPrefab;
+    public float anvilProbability;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("StartGame");
     }
 
+    IEnumerator StartGame()
+    {
+        //Countdown
+        for (int i = 3; i >= 1; i--)
+        {
+            Debug.Log(i); //insert countdown here
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("Start!"); //insert Start! here
+    }
     // Update is called once per frame
     void Update()
     {
