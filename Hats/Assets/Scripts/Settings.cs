0e2fa52779296ecec9748fd3076928026ee4d6f9
BrookeAug1 <44063647+BrookeAug1@users.noreply.github.com>
2019-02-26 10:24:16 -0500

    stuff

---
+++ Hats/Assets/Scripts/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings
+{
+    public bool music;
+    public bool sound;
+
+    void Start()
+    {
+        music = true;
+        sound = true;
+    }
+    void ToggleMusic()
+    {
+        if (music)
+            music = false;
+        else
+            music = true;
+    }
+    void ToggleSound()
+    {
+        if (sound)
+            sound = false;
+        else
+            sound = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
