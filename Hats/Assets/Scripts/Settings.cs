dadbfb8edb6f12d6c69d70045db6cffcb7feb2fb
BrookeAug1 <44063647+BrookeAug1@users.noreply.github.com>
2019-02-26 11:08:55 -0500

    button On Click

--- Hats/Assets/Scripts/Settings.cs
+++ Hats/Assets/Scripts/Settings.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Settings
 {
+    public Button startB;
+    public Button soundB;
+    public Button musicB;
+    public Button achieveB;
+    public Button leaderB;
+    public Button infoB;
+    public Button resetB;
+    public Button resetY;
+    public Button resetN;
+
     public bool music;
     public bool sound;
 
@@ -11,20 +23,58 @@ public class Settings
     {
         music = true;
         sound = true;
+        startB.onClick.AddListener(StartGame);
+        soundB.onClick.AddListener(ToggleSound);
+        musicB.onClick.AddListener(ToggleMusic);
+        achieveB.onClick.AddListener(ShowAchieve);
+        leaderB.onClick.AddListener(ShowLeader);
+        infoB.onClick.AddListener(ShowInfo);
+        resetB.onClick.AddListener(ResetCheck);
+        resetY.onClick.AddListener(ResetYes);
+        resetN.onClick.AddListener(ResetNo);
     }
-    void ToggleMusic()
+
+    public void ResetCheck()
+    {
+        
+    }
+    public void ResetYes()
+    {
+
+    }
+    public void ResetNo()
+    {
+
+    }
+    public void ShowInfo()
+    {
+
+    }
+    public void ShowAchieve()
+    {
+
+    }
+    public void ShowLeader()
+    {
+
+    }
+    public void ToggleMusic()
     {
         if (music)
             music = false;
         else
             music = true;
     }
-    void ToggleSound()
+    public void ToggleSound()
     {
         if (sound)
             sound = false;
         else
             sound = true;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
     }
     // Update is called once per frame
     void Update()
