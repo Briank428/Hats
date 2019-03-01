0e31625cb64358c74dd92d4428aca580adaceaf8
Briank428 <44040150+Briank428@users.noreply.github.com>
2019-02-28 19:10:10 -0500

    hat spawning and time implementation started

--- Hats/Assets/Scripts/GameManager.cs
+++ Hats/Assets/Scripts/GameManager.cs
@@ -1,38 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [System.Serializable]
-public struct Hats
+public struct Hat
 {
     public string type;
     public Sprite image;
-    public float probability;
+    public float skipProbability;
 }
 public class GameManager : MonoBehaviour
 {
-    public GameObject playerPrefab;
     [SerializeField]
-    public List<Hats> hatTypes = new List<Hats>();
+    public List<Hat> hatTypes = new List<Hat>();
     public GameObject anvilPrefab;
-    public float anvilProbability;
-    private const float SPEED_INIT = 5f; //initial time between drops
+    public const float ANVIL_PROBABILITY = 0.3f;
+    private const float SPEED_INIT = 5.0f; //initial time between drops
     private const float MAX_SPEED = 0.1f; //max hat drop speed
+    private const float SPEED_DECREMENT = 0.1f;
     private float currentSpeed; //stores current time between drops
+    public Player playerPrefab;
+    private Player playerInstance;
     private int numHatsCollected;
-    private GameObject playerInstance;
+    private int numLives;
+    private GameManager gmInstance;
+    private static System.Random random = new System.Random();
 
     // Start is called before the first frame update
     void Start()
     {
+        numLives = 10;
+        currentSpeed = SPEED_INIT;
         StartCoroutine("StartGame");
+        gmInstance = this;
     }
 
     IEnumerator StartGame()
     {
         //spawn player
-        playerInstance = Instantiate(playerPrefab) as GameObject;
-        playerInstance.transform.position = new Vector3();
+        playerInstance = Instantiate(playerPrefab) as Player;
+        playerInstance.transform.position = Vector3.zero;
         //Countdown
         for (int i = 3; i >= 1; i--)
         {
@@ -40,10 +48,41 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(1);
         }
         Debug.Log("Start!"); //insert Start! here
+
+        while (numLives != 0)
+        {
+            yield return new WaitForSeconds(currentSpeed);
+            if (random.Next(0,1) < ANVIL_PROBABILITY) SpawnAnvil();
+            else { SpawnHat(); }
+        }
+    }
+    void SpawnHat()
+    {
+        //choose hat to spawn
+        Hat temp;
+        do
+        {
+            temp = hatTypes[random.Next(hatTypes.Count)];
+        } while (random.Next(0,1) < temp.skipProbability);
+        
+        //spawn hat
+
+
+    }
+
+    void SpawnAnvil()
+    {
+        
+    }
+
+    public void HatCollected()
+    {
+        numHatsCollected++;
+        if (numHatsCollected % 10 == 0 && currentSpeed > MAX_SPEED) currentSpeed -= SPEED_DECREMENT;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (numLives == 0) StopAllCoroutines();
     }
 }
