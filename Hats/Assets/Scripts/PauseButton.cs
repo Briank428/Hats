1d65d636873677e7f43825d6b8af30b14cad99f6
BrookeAug1 <44063647+BrookeAug1@users.noreply.github.com>
2019-03-07 14:53:30 -0500

    f

---
+++ Hats/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+    public Button pauseB;
+    public Button playB;
+    public Button musicB;
+    public Button soundB;
+    public Image pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePanel.gameObject.SetActive(false);
+        pauseB.onClick.AddListener(OpenPause);
+        playB.onClick.AddListener(ClosePause);
+    }
+
+    public void OpenPause() { TogglePause();  pausePanel.gameObject.SetActive(true); }
+    public void ClosePause() { TogglePause();  pausePanel.gameObject.SetActive(false); }
+}
